6be055a99bb68a7d779527b5a02b4b25d4891461
MoxitPanchal <128708440+MoxitPanchal@users.noreply.github.com>
2024-03-12 18:43:58 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-sudo su<< EOF
 sudo apt-get update
 sudo apt-get install libpcap-dev libpcap0.8 libpcap0.8-dev -y
+
+# Install sublist3r
+sudo apt-get install sublist3r -y
+
+# Install subfinder
+sudo apt-get install subfinder -y
+
+# Install curl
+sudo apt-get install curl -y
 
 # Check if Go is installed by running "go version" command
 if ! command -v go &> /dev/null; then
     # Go is not installed, so proceed with installation
     wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz
     sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
+    export PATH=$PATH:/usr/local/go/bin
     echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zshrc
-    source ~/.zshrc
+    echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
     rm go1.22.1.linux-amd64.tar.gz
     
     # Verify installation
@@ -22,15 +31,6 @@ if ! command -v go &> /dev/null; then
 else
     echo "Go is already installed."
 fi
-
-# Install sublist3r
-sudo apt-get install sublist3r -y
-
-# Install subfinder
-sudo apt-get install subfinder -y
-
-# Install curl
-sudo apt-get install curl -y
 
 # Install naabu
 go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
@@ -45,5 +45,6 @@ rm -rf massdns
 
 # Install puredns
 go install github.com/d3mondev/puredns/v2@latest
-export PATH=$PATH:/root/go/bin
-EOF
+export PATH=\$PATH:$(go env GOPATH)/bin
+echo "export PATH=\$PATH:$(go env GOPATH)/bin" >> ~/.bashrc
+echo "export PATH=\$PATH:$(go env GOPATH)/bin" >> ~/.zshrc
