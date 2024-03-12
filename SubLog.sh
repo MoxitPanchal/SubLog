b23f4ec0c7a22d3b4588436acc27138df88a505f
MoxitPanchal <128708440+MoxitPanchal@users.noreply.github.com>
2024-03-12 14:25:47 +0530

    SubLog and files added

---
+++ SubLog.sh
@@ -0,0 +1,223 @@
+#!/bin/bash
+clear
+echo ""
+echo  -e "[38;2;121;240;21m [39m[38;2;125;239;19m [39m[38;2;130;236;16m█[39m[38;2;134;234;14m█[39m[38;2;138;232;13m█[39m[38;2;142;229;11m█[39m[38;2;147;227;9m█[39m[38;2;151;224;8m█[39m[38;2;155;221;6m█[39m[38;2;159;218;5m█[39m[38;2;163;215;4m [39m[38;2;167;212;3m [39m[38;2;171;209;2m [39m[38;2;175;206;2m [39m[38;2;179;202;1m [39m[38;2;183;199;1m [39m[38;2;187;195;1m [39m[38;2;190;192;1m [39m[38;2;194;188;1m [39m[38;2;197;184;1m█[39m[38;2;201;180;1m█[39m[38;2;204;177;2m [39m[38;2;208;173;2m [39m[38;2;211;169;3m [39m[38;2;214;165;4m [39m[38;2;217;161;5m [39m[38;2;220;156;6m [39m[38;2;223;152;7m█[39m[38;2;226;148;9m█[39m[38;2;228;144;10m [39m[38;2;231;140;12m [39m[38;2;233;136;14m [39m[38;2;236;131;16m [39m[38;2;238;127;18m [39m[38;2;240;123;20m [39m[38;2;242;119;22m [39m[38;2;243;114;25m [39m[38;2;245;110;27m [39m[38;2;247;106;30m [39m[38;2;248;102;33m [39m[38;2;249;98;35m [39m[38;2;250;94;38m [39m[38;2;251;90;41m [39m[38;2;252;86;45m [39m[38;2;253;82;48m [39m[38;2;254;78;51m [39m[38;2;254;74;55m [39m[38;2;254;70;58m [39m[38;2;254;66;62m [39m[38;2;254;63;65m [39m[38;2;254;59;69m [39m[38;2;254;55;73m [39m[38;2;254;52;77m[39m
+[38;2;134;234;14m [39m[38;2;138;232;13m█[39m[38;2;142;229;11m█[39m[38;2;147;227;9m░[39m[38;2;151;224;8m░[39m[38;2;155;221;6m░[39m[38;2;159;218;5m░[39m[38;2;163;215;4m░[39m[38;2;167;212;3m░[39m[38;2;171;209;2m [39m[38;2;175;206;2m [39m[38;2;179;202;1m [39m[38;2;183;199;1m [39m[38;2;187;195;1m [39m[38;2;190;192;1m [39m[38;2;194;188;1m [39m[38;2;197;184;1m [39m[38;2;201;180;1m [39m[38;2;204;177;2m░[39m[38;2;208;173;2m█[39m[38;2;211;169;3m█[39m[38;2;214;165;4m [39m[38;2;217;161;5m [39m[38;2;220;156;6m [39m[38;2;223;152;7m [39m[38;2;226;148;9m [39m[38;2;228;144;10m░[39m[38;2;231;140;12m█[39m[38;2;233;136;14m█[39m[38;2;236;131;16m [39m[38;2;238;127;18m [39m[38;2;240;123;20m [39m[38;2;242;119;22m [39m[38;2;243;114;25m [39m[38;2;245;110;27m [39m[38;2;247;106;30m [39m[38;2;248;102;33m [39m[38;2;249;98;35m [39m[38;2;250;94;38m [39m[38;2;251;90;41m [39m[38;2;252;86;45m [39m[38;2;253;82;48m [39m[38;2;254;78;51m [39m[38;2;254;74;55m [39m[38;2;254;70;58m [39m[38;2;254;66;62m [39m[38;2;254;63;65m█[39m[38;2;254;59;69m█[39m[38;2;254;55;73m█[39m[38;2;254;52;77m█[39m[38;2;253;49;81m█[39m[38;2;252;45;85m [39m[38;2;252;42;89m[39m
+[38;2;147;227;9m░[39m[38;2;151;224;8m█[39m[38;2;155;221;6m█[39m[38;2;159;218;5m [39m[38;2;163;215;4m [39m[38;2;167;212;3m [39m[38;2;171;209;2m [39m[38;2;175;206;2m [39m[38;2;179;202;1m [39m[38;2;183;199;1m [39m[38;2;187;195;1m [39m[38;2;190;192;1m█[39m[38;2;194;188;1m█[39m[38;2;197;184;1m [39m[38;2;201;180;1m [39m[38;2;204;177;2m [39m[38;2;208;173;2m█[39m[38;2;211;169;3m█[39m[38;2;214;165;4m░[39m[38;2;217;161;5m█[39m[38;2;220;156;6m█[39m[38;2;223;152;7m [39m[38;2;226;148;9m [39m[38;2;228;144;10m [39m[38;2;231;140;12m [39m[38;2;233;136;14m [39m[38;2;236;131;16m░[39m[38;2;238;127;18m█[39m[38;2;240;123;20m█[39m[38;2;242;119;22m [39m[38;2;243;114;25m [39m[38;2;245;110;27m [39m[38;2;247;106;30m [39m[38;2;248;102;33m [39m[38;2;249;98;35m [39m[38;2;250;94;38m [39m[38;2;251;90;41m [39m[38;2;252;86;45m█[39m[38;2;253;82;48m█[39m[38;2;254;78;51m█[39m[38;2;254;74;55m█[39m[38;2;254;70;58m█[39m[38;2;254;66;62m█[39m[38;2;254;63;65m [39m[38;2;254;59;69m [39m[38;2;254;55;73m█[39m[38;2;254;52;77m█[39m[38;2;253;49;81m░[39m[38;2;252;45;85m░[39m[38;2;252;42;89m░[39m[38;2;251;39;93m█[39m[38;2;250;36;97m█[39m[38;2;248;33;101m[39m
+[38;2;159;218;5m░[39m[38;2;163;215;4m█[39m[38;2;167;212;3m█[39m[38;2;171;209;2m█[39m[38;2;175;206;2m█[39m[38;2;179;202;1m█[39m[38;2;183;199;1m█[39m[38;2;187;195;1m█[39m[38;2;190;192;1m█[39m[38;2;194;188;1m█[39m[38;2;197;184;1m░[39m[38;2;201;180;1m█[39m[38;2;204;177;2m█[39m[38;2;208;173;2m [39m[38;2;211;169;3m [39m[38;2;214;165;4m░[39m[38;2;217;161;5m█[39m[38;2;220;156;6m█[39m[38;2;223;152;7m░[39m[38;2;226;148;9m█[39m[38;2;228;144;10m█[39m[38;2;231;140;12m█[39m[38;2;233;136;14m█[39m[38;2;236;131;16m█[39m[38;2;238;127;18m█[39m[38;2;240;123;20m [39m[38;2;242;119;22m░[39m[38;2;243;114;25m█[39m[38;2;245;110;27m█[39m[38;2;247;106;30m [39m[38;2;248;102;33m [39m[38;2;249;98;35m [39m[38;2;250;94;38m [39m[38;2;251;90;41m [39m[38;2;252;86;45m [39m[38;2;253;82;48m [39m[38;2;254;78;51m█[39m[38;2;254;74;55m█[39m[38;2;254;70;58m░[39m[38;2;254;66;62m░[39m[38;2;254;63;65m░[39m[38;2;254;59;69m░[39m[38;2;254;55;73m█[39m[38;2;254;52;77m█[39m[38;2;253;49;81m░[39m[38;2;252;45;85m█[39m[38;2;252;42;89m█[39m[38;2;251;39;93m [39m[38;2;250;36;97m [39m[38;2;248;33;101m░[39m[38;2;247;30;105m█[39m[38;2;245;28;109m█[39m[38;2;244;25;113m[39m
+[38;2;171;209;2m░[39m[38;2;175;206;2m░[39m[38;2;179;202;1m░[39m[38;2;183;199;1m░[39m[38;2;187;195;1m░[39m[38;2;190;192;1m░[39m[38;2;194;188;1m░[39m[38;2;197;184;1m░[39m[38;2;201;180;1m█[39m[38;2;204;177;2m█[39m[38;2;208;173;2m░[39m[38;2;211;169;3m█[39m[38;2;214;165;4m█[39m[38;2;217;161;5m [39m[38;2;220;156;6m [39m[38;2;223;152;7m░[39m[38;2;226;148;9m█[39m[38;2;228;144;10m█[39m[38;2;231;140;12m░[39m[38;2;233;136;14m█[39m[38;2;236;131;16m█[39m[38;2;238;127;18m░[39m[38;2;240;123;20m░[39m[38;2;242;119;22m░[39m[38;2;243;114;25m█[39m[38;2;245;110;27m█[39m[38;2;247;106;30m░[39m[38;2;248;102;33m█[39m[38;2;249;98;35m█[39m[38;2;250;94;38m [39m[38;2;251;90;41m [39m[38;2;252;86;45m [39m[38;2;253;82;48m [39m[38;2;254;78;51m [39m[38;2;254;74;55m [39m[38;2;254;70;58m░[39m[38;2;254;66;62m█[39m[38;2;254;63;65m█[39m[38;2;254;59;69m [39m[38;2;254;55;73m [39m[38;2;254;52;77m [39m[38;2;253;49;81m░[39m[38;2;252;45;85m█[39m[38;2;252;42;89m█[39m[38;2;251;39;93m░[39m[38;2;250;36;97m░[39m[38;2;248;33;101m█[39m[38;2;247;30;105m█[39m[38;2;245;28;109m█[39m[38;2;244;25;113m█[39m[38;2;242;23;118m█[39m[38;2;240;20;122m█[39m[38;2;238;18;126m[39m
+[38;2;183;199;1m [39m[38;2;187;195;1m [39m[38;2;190;192;1m [39m[38;2;194;188;1m [39m[38;2;197;184;1m [39m[38;2;201;180;1m [39m[38;2;204;177;2m [39m[38;2;208;173;2m░[39m[38;2;211;169;3m█[39m[38;2;214;165;4m█[39m[38;2;217;161;5m░[39m[38;2;220;156;6m█[39m[38;2;223;152;7m█[39m[38;2;226;148;9m [39m[38;2;228;144;10m [39m[38;2;231;140;12m░[39m[38;2;233;136;14m█[39m[38;2;236;131;16m█[39m[38;2;238;127;18m░[39m[38;2;240;123;20m█[39m[38;2;242;119;22m█[39m[38;2;243;114;25m [39m[38;2;245;110;27m [39m[38;2;247;106;30m░[39m[38;2;248;102;33m█[39m[38;2;249;98;35m█[39m[38;2;250;94;38m░[39m[38;2;251;90;41m█[39m[38;2;252;86;45m█[39m[38;2;253;82;48m [39m[38;2;254;78;51m [39m[38;2;254;74;55m [39m[38;2;254;70;58m [39m[38;2;254;66;62m [39m[38;2;254;63;65m [39m[38;2;254;59;69m░[39m[38;2;254;55;73m█[39m[38;2;254;52;77m█[39m[38;2;253;49;81m [39m[38;2;252;45;85m [39m[38;2;252;42;89m [39m[38;2;251;39;93m░[39m[38;2;250;36;97m█[39m[38;2;248;33;101m█[39m[38;2;247;30;105m [39m[38;2;245;28;109m░[39m[38;2;244;25;113m░[39m[38;2;242;23;118m░[39m[38;2;240;20;122m░[39m[38;2;238;18;126m░[39m[38;2;236;16;130m█[39m[38;2;234;14;135m█[39m[38;2;231;12;139m[39m
+[38;2;194;188;1m [39m[38;2;197;184;1m█[39m[38;2;201;180;1m█[39m[38;2;204;177;2m█[39m[38;2;208;173;2m█[39m[38;2;211;169;3m█[39m[38;2;214;165;4m█[39m[38;2;217;161;5m█[39m[38;2;220;156;6m█[39m[38;2;223;152;7m [39m[38;2;226;148;9m░[39m[38;2;228;144;10m░[39m[38;2;231;140;12m█[39m[38;2;233;136;14m█[39m[38;2;236;131;16m█[39m[38;2;238;127;18m█[39m[38;2;240;123;20m█[39m[38;2;242;119;22m█[39m[38;2;243;114;25m░[39m[38;2;245;110;27m█[39m[38;2;247;106;30m█[39m[38;2;248;102;33m█[39m[38;2;249;98;35m█[39m[38;2;250;94;38m█[39m[38;2;251;90;41m█[39m[38;2;252;86;45m [39m[38;2;253;82;48m░[39m[38;2;254;78;51m█[39m[38;2;254;74;55m█[39m[38;2;254;70;58m█[39m[38;2;254;66;62m█[39m[38;2;254;63;65m█[39m[38;2;254;59;69m█[39m[38;2;254;55;73m█[39m[38;2;254;52;77m█[39m[38;2;253;49;81m░[39m[38;2;252;45;85m░[39m[38;2;252;42;89m█[39m[38;2;251;39;93m█[39m[38;2;250;36;97m█[39m[38;2;248;33;101m█[39m[38;2;247;30;105m█[39m[38;2;245;28;109m█[39m[38;2;244;25;113m [39m[38;2;242;23;118m [39m[38;2;240;20;122m [39m[38;2;238;18;126m█[39m[38;2;236;16;130m█[39m[38;2;234;14;135m█[39m[38;2;231;12;139m█[39m[38;2;229;11;143m█[39m[38;2;226;9;147m [39m[38;2;224;8;151m[39m
+[38;2;204;177;2m░[39m[38;2;208;173;2m░[39m[38;2;211;169;3m░[39m[38;2;214;165;4m░[39m[38;2;217;161;5m░[39m[38;2;220;156;6m░[39m[38;2;223;152;7m░[39m[38;2;226;148;9m░[39m[38;2;228;144;10m [39m[38;2;231;140;12m [39m[38;2;233;136;14m [39m[38;2;236;131;16m░[39m[38;2;238;127;18m░[39m[38;2;240;123;20m░[39m[38;2;242;119;22m░[39m[38;2;243;114;25m░[39m[38;2;245;110;27m░[39m[38;2;247;106;30m [39m[38;2;248;102;33m░[39m[38;2;249;98;35m░[39m[38;2;250;94;38m░[39m[38;2;251;90;41m░[39m[38;2;252;86;45m░[39m[38;2;253;82;48m [39m[38;2;254;78;51m [39m[38;2;254;74;55m [39m[38;2;254;70;58m░[39m[38;2;254;66;62m░[39m[38;2;254;63;65m░[39m[38;2;254;59;69m░[39m[38;2;254;55;73m░[39m[38;2;254;52;77m░[39m[38;2;253;49;81m░[39m[38;2;252;45;85m░[39m[38;2;252;42;89m [39m[38;2;251;39;93m [39m[38;2;250;36;97m░[39m[38;2;248;33;101m░[39m[38;2;247;30;105m░[39m[38;2;245;28;109m░[39m[38;2;244;25;113m░[39m[38;2;242;23;118m░[39m[38;2;240;20;122m [39m[38;2;238;18;126m [39m[38;2;236;16;130m [39m[38;2;234;14;135m░[39m[38;2;231;12;139m░[39m[38;2;229;11;143m░[39m[38;2;226;9;147m░[39m[38;2;224;8;151m░[39m[38;2;221;6;156m [39m[38;2;218;5;160m [39m[38;2;215;4;164m[39m"
+echo " "
+# Define color variables
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+BLUE='\033[1;34m'
+YELLOW='\033[1;33m'
+CYAN='\033[1;36m'
+PURPLE='\033[1;35m'
+NC='\033[0m' # No Color
+
+# Function to display script usage
+usage() {
+    echo "Usage: $0 [-w wordlist_file] <domain>"
+    exit 1
+}
+
+# Ask for folder name
+echo -n -e "${YELLOW}Enter the folder name to save the text files: ${NC}" 
+read folder_name
+
+# Check if folder name is provided
+if [ -z "$folder_name" ]; then
+    echo -e "${RED}Error: Folder name cannot be empty.${NC}"
+    exit 1
+fi
+
+# Check if folder exists, if not create it
+if [ ! -d "$folder_name" ]; then
+    mkdir "$folder_name"
+fi
+
+# Change to the folder directory
+cd "$folder_name" || exit 1
+
+# Check if at least domain is provided as an argument
+if [ $# -lt 1 ]; then
+    usage
+fi
+
+# Parse options
+while getopts ":w:" opt; do
+    case ${opt} in
+        w )
+            wordlist="$OPTARG"
+            ;;
+        \? )
+            echo "Invalid option: $OPTARG" 1>&2
+            usage
+            ;;
+        : )
+            echo "Option -$OPTARG requires an argument." 1>&2
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+# Check if domain is provided
+if [ $# -ne 1 ]; then
+    usage
+fi
+
+domain="$1"
+
+# Default wordlist if not provided
+if [ -z "$wordlist" ]; then
+    wordlist="../default_wordlist.txt"
+fi
+
+# Step 1: Find subdomains using sublist3r
+echo
+echo -e  "${BLUE}[1]${NC} ${YELLOW}Finding subdomains with sublist3r.${NC}"
+python -c "import sublist3r;subdomains = sublist3r.main('$domain', 50, 'sublist3r.tmp.txt' ,ports= None, silent=True, verbose= False, enable_bruteforce= False, engines=None)"
+
+# Step 2: Find subdomains using subfinder
+echo
+echo -e -n "${BLUE}[2]${NC} ${YELLOW}Finding subdomains with subfinder.${NC}"
+echo
+subfinder  -silent  -d $domain > subfinder.tmp.txt
+
+# Step 3: Perform subdomain brute-force with puredns
+echo
+echo -e  "${BLUE}[3]${NC} ${YELLOW}Subdomain bruteforce using puredns.${NC}"
+echo -e "${RED}NOTE: ${NC}This might take some time (30-35 mins)."
+# Define the folder containing the lists
+folder="../subdomain-wordlists"
+counter=0
+# Iterate over each file in the folder
+for list in "$folder"/*; do
+    # Check if the file exists and is a regular file
+    if [ -f "$list" ]; then
+        counter=$((counter + 1))
+        # Run PureDNS command with the current file
+         puredns -q bruteforce --rate-limit-trusted 0 --resolvers-trusted ../resolv.txt $list --trusted-only $domain > puredns-file-${counter}.txt
+        # Print message indicating completion
+        echo
+        echo -e "${YELLOW}[+]${NC} ${GREEN}Subdomain bruteforce wordlist ${counter} completed. 🗸${NC}"
+        echo
+        sleep 10
+    fi
+done
+cat puredns-file-* | sort | uniq > puredns.tmp.txt
+rm puredns-file-*
+
+# Step 4: Find subdomains using crt.sh
+echo
+echo -e "${BLUE}[4]${NC} ${YELLOW} Extracting subdomains from crt.sh.${NC}"
+echo
+curl -s "https://crt.sh/?q=%.$domain&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u  >  crtsh.tmp.txt
+
+# Step 5: Make a list of all unique subdomains from the temporary files
+echo -e -n "${BLUE}[5]${NC} ${YELLOW} Combining all subdomains in subdomains.txt${NC}"
+cat sublist3r.tmp.txt subfinder.tmp.txt  puredns.tmp.txt crtsh.tmp.txt| sort | uniq > subdomains.txt
+echo
+echo -e "${YELLOW}Found subdomains are saved in subdomains.txt.${NC}"
+# Step 6: Perform port scanning with naabu
+echo
+echo -e "${BLUE}[6]${NC} ${YELLOW} Scanning ports on each subdomain using naabu${NC}"
+echo
+naabu -silent -c 50 -l subdomains.txt -p 1-65535 > subdomain-port.tmp.txt
+
+# Step 7: Check accessibility of subdomains using curl
+echo -e "${BLUE}[7]${NC} ${YELLOW} Checking accessibilty of found subdomains with curl.${NC}"
+echo
+# Define input file containing subdomains and ports
+INPUT_FILE="subdomain-port.tmp.txt"
+
+# Define output files for HTTP and HTTPS accessible subdomains
+HTTP_OUTPUT_FILE="http_accessible.txt"
+HTTPS_OUTPUT_FILE="https_accessible.txt"
+
+# Check if input file exists
+if [ ! -f "$INPUT_FILE" ]; then
+    echo "Error: Input file '$INPUT_FILE' not found."
+    exit 1
+fi
+
+# Loop through each subdomain:port combination in the input file
+while IFS= read -r subdomainwithport; do
+    echo "Checking $subdomainwithport"
+    echo
+    # Check HTTP accessibility
+    response_code=$(curl -s -o /dev/null -w "%{http_code}" --connect-timeout 10 --max-time 20 "http://$subdomainwithport")
+    if [[ $response_code == 2* || $response_code == 3* ]]; then
+        echo "$subdomainwithport" >> "$HTTP_OUTPUT_FILE"
+        echo -e "${GREEN}HTTP access successful${NC}"
+    else
+        echo -e "${RED}HTTP access failed${NC}"
+    fi
+
+    # Check HTTPS accessibility
+    response_code=$(curl -s -o /dev/null -w "%{http_code}" --connect-timeout 10 --max-time 20 "https://$subdomainwithport")
+    if [[ $response_code == 2* || $response_code == 3* ]]; then
+        echo "$subdomainwithport" >> "$HTTPS_OUTPUT_FILE"
+        echo -e "${GREEN}HTTPS access successful${NC}"
+    else
+        echo -e "${RED}HTTPS access failed${NC}"
+    fi
+
+    echo "------------------------------------------"
+done < "$INPUT_FILE"
+# Add 'http://' at the start of each line in HTTP accessible file
+sed -i 's/^/http:\/\//' "$HTTP_OUTPUT_FILE"
+
+# Add 'https://' at the start of each line in HTTPS accessible file
+sed -i 's/^/https:\/\//' "$HTTPS_OUTPUT_FILE"
+echo
+echo -e "${YELLOW}HTTP accessible subdomains saved to '$HTTP_OUTPUT_FILE'"
+echo -e
+echo -e "HTTPS accessible subdomains saved to '$HTTPS_OUTPUT_FILE'${NC}"
+
+# Cleanup temporary files
+rm sublist3r.tmp.txt subfinder.tmp.txt puredns.tmp.txt crtsh.tmp.txt subdomain-port.tmp.txt
+
+# Step 8: Check if HTTP accessible subdomains redirect to HTTPS
+# Define output file for final HTTP accessible subdomains
+echo
+echo -e "${BLUE}[8]${NC}${YELLOW} Checking for HTTP redirection.${NC}"
+FINAL_HTTP_OUTPUT_FILE="final_http_accessible.txt"
+
+# Create an empty array to store redirected URLs
+redirected_urls=()
+echo
+# Loop through each HTTP accessible subdomain
+while IFS= read -r http_subdomain; do
+    echo -e "Checking redirection for ${CYAN}$http_subdomain${NC}"
+    # Perform curl request to check if HTTP redirects to HTTPS
+    redirected_to=$(curl -s --head -o /dev/null -w "%{redirect_url}" --connect-timeout 10 --max-time 30 "$http_subdomain")
+
+    # Check if the redirection URL starts with HTTPS
+    if [[ "$redirected_to" =~ ^https:// ]]; then
+        echo -e "${GREEN}Redirection to HTTPS found${NC}, removing $http_subdomain from final HTTP list."
+        echo -e "Redirected to: $redirected_to"
+        redirected_urls+=("$http_subdomain")
+    else
+        echo "${RED}No redirection to HTTPS found${NC}, keeping $http_subdomain"
+    fi
+
+    echo "------------------------------------------------------------------------------------------------------------------------"
+done < "$HTTP_OUTPUT_FILE"
+
+# Remove redirected URLs from the final HTTP list
+awk 'NR==FNR{a[$0];next} !($0 in a)' <(printf '%s\n' "${redirected_urls[@]}") "$HTTP_OUTPUT_FILE" > "$FINAL_HTTP_OUTPUT_FILE"
+echo
+echo -e "${PURPLE}Final HTTP accessible subdomains saved to '$FINAL_HTTP_OUTPUT_FILE'"
+echo -e
+echo -e "HTTPS accessible subdomains saved to '$HTTPS_OUTPUT_FILE'${NC}"
+rm http_accessible.txt
+#Step 9: Combine all accessible subdomains
+cat final_http_accessible.txt https_accessible.txt | sort | uniq > $domain-subdomains.txt
+echo
+echo -e "${YELLOW}Subdomain enumeration completed. Results saved in $domain-subdomains.txt${NC}"
---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+sudo su<< EOF
+sudo apt-get update
+sudo apt-get install libpcap-dev libpcap0.8 libpcap0.8-dev -y
+
+# Check if Go is installed by running "go version" command
+if ! command -v go &> /dev/null; then
+    # Go is not installed, so proceed with installation
+    wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz
+    sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
+    echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zshrc
+    source ~/.zshrc
+    rm go1.22.1.linux-amd64.tar.gz
+    
+    # Verify installation
+    if ! command -v go &> /dev/null; then
+        echo "Error: Go installation failed."
+        exit 1
+    else
+        echo "Go installed successfully."
+    fi
+else
+    echo "Go is already installed."
+fi
+
+# Install sublist3r
+sudo apt-get install sublist3r -y
+
+# Install subfinder
+sudo apt-get install subfinder -y
+
+# Install curl
+sudo apt-get install curl -y
+
+# Install naabu
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+
+# Install massdns
+git clone https://github.com/blechschmidt/massdns.git
+cd massdns
+make
+sudo cp -r bin/* /usr/local/bin/
+cd ..
+rm -rf massdns
+
+# Install puredns
+go install github.com/d3mondev/puredns/v2@latest
+export PATH=$PATH:/root/go/bin
+EOF
