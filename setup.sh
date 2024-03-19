dc0d0482e076715999328bf5f98487de01e5284b
MoxitPanchal <128708440+MoxitPanchal@users.noreply.github.com>
2024-03-19 17:22:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,12 +14,12 @@ sudo apt-get install curl -y
 # Check if Go is installed by running "go version" command
 if ! command -v go &> /dev/null; then
     # Go is not installed, so proceed with installation
-    wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz
+    sudo wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz
     sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
     export PATH=$PATH:/usr/local/go/bin
     echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zshrc
     echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
-    rm go1.22.1.linux-amd64.tar.gz
+    sudo rm go1.22.1.linux-amd64.tar.gz
     
     # Verify installation
     if ! command -v go &> /dev/null; then
@@ -36,16 +36,16 @@ fi
 go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
 
 # Install massdns
-git clone https://github.com/blechschmidt/massdns.git
+sudo git clone https://github.com/blechschmidt/massdns.git
 cd massdns
-make
+sudo make
 sudo cp -r bin/* /usr/local/bin/
 cd ..
-rm -rf massdns
+sudo rm -rf massdns
 
 # Install puredns
 go install github.com/d3mondev/puredns/v2@latest
 export PATH=$PATH:$(go env GOPATH)/bin
 echo "export PATH=$PATH:$(go env GOPATH)/bin" >> ~/.bashrc
 echo "export PATH=$PATH:$(go env GOPATH)/bin" >> ~/.zshrc
-source ~/.zshrc
+source ~/.profile
