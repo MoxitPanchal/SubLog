2624e2dd3925adb4475d363bf3aff9535607e394
MoxitPanchal <edu.moxit@gmail.com>
2024-05-02 11:56:34 +0530

    Changed go installation method and path names

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo zsh <<EOF
 sudo apt-get update
 sudo apt-get install libpcap-dev libpcap0.8 libpcap0.8-dev jq sed -y
 
@@ -14,11 +13,7 @@ sudo apt-get install curl -y
 
 # Check if Go is installed by running "go version" command
 if ! command -v go &> /dev/null; then
-    # Go is not installed, so proceed with installation
-    sudo wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz
-    sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
-    export PATH=$PATH:/usr/local/go/bin
-    sudo rm -rf go1.22.1.linux-amd64.tar.gz
+    sudo apt install golang-go -y
         
     # Verify installation
     if ! command -v go &> /dev/null; then
@@ -45,15 +40,9 @@ sudo rm -rf massdns
 # Install puredns
 go install github.com/d3mondev/puredns/v2@latest
 export PATH=$PATH:~/go/bin
-echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.zshrc
-echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bashrc
-EOF
 sleep 3
 sudo cp -r /root/go $HOME/
 export PATH=$PATH:/usr/local/go/bin:~/go/bin
-echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.zshrc
-echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bashrc
-source ~/.zshrc
 if ! command -v puredns &> /dev/null; then
     echo "Error installing puredns!"
 else
